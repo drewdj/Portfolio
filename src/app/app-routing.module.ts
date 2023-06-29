59443c7118ddf0578dfe9a527a8fddad52bd65c0
David Jiménez Monterrubio <28drew05@gmail.com>
2023-06-29 12:34:32 +0200

    Initial commit, angular proyect + routing, tailwind + daisyui added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import {RouterModule,Routes} from "@angular/router";
+
+import {NavBarComponent} from "./components/nav-bar/nav-bar.component";
+
+
+const routes: Routes = [
+  {path: '',component: NavBarComponent},
+
+
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
